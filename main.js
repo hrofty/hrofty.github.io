ecc488f2e7f4efcfb8cc42ae95a2b6c1c634da77
Andrey Panitkov <Andrey.Panitkov@broit.io>
2023-06-23 22:23:18 +0200

    added node_modules for now

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // npm run dev
 
-import * as THREE from './node_modules/three'
+import * as THREE from 'three/build/three'
 import "./style.css"
-import { FirstPersonControls } from './node_modules/three/examples/jsm/controls/FirstPersonControls.js'
+import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls'
 
 class Microdistrict {
   constructor() {
